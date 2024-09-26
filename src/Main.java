294c223d7186cb28268e71d4144a797d291e179b
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-09-26 16:08:24 +0200

    create and loop for hashmap

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.HashMap;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//        Add Elements to HashMap: Create a HashMap, add 5 key-value pairs, and print them.
+        HashMap<Integer,String> map = new HashMap<>();
+        map.put(1,"product1");
+        map.put(2,"product2");
+        map.put(3,"product3");
+        map.put(4,"product4");
+        map.put(5,"product5");
+        map.forEach((key,value) -> System.out.println("number of product : "+key+" product name : "+value));
+
+
+
+
+
+
+
+
+
     }
 }
